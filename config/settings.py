1ac44b2e883ca42600fd9fa6e33ee7dd97234ed1
Nasrin <a.navabi65@gmail.com>
2022-11-14 19:00:32 +0100

    add middleware

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # extra code
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
