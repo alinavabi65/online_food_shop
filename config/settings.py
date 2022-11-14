4bb6fee2ecb423c110ace44994260902229486d0
Nasrin <a.navabi65@gmail.com>
2022-11-14 18:15:16 +0100

    modified static file

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # static file Config
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # media file config
 MEDIA_URL = '/media/'
