71a6ae3857d9972737900a10fd8a53970d819eb9
Nasrin <a.navabi65@gmail.com>
2022-11-15 10:57:38 +0100

    edit

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 from .cart import Cart
 from .forms import AddToCartProductForm
@@ -42,6 +43,19 @@ def remove_from_cart(request, product_id):
     cart.remove(product)
 
     return redirect('cart:cart_detail')
+
+
+@require_POST
+def clear_cart(request):
+    cart = Cart(request)
+
+    if len(cart):
+        cart.clear()
+        messages.success(request, 'All products successfully removed from your cart')
+    else:
+        messages.warning(request, 'Your cart is already empty')
+
+    return redirect('product_list')
 
 
 
