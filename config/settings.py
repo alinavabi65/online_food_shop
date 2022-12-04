5885001039dd9c66b6ba3868623057d1362a2895
Nasrin <a.navabi65@gmail.com>
2022-12-04 09:28:44 +0100

    modified problem

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # extra code
+    # extra code for heroku deploy
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
